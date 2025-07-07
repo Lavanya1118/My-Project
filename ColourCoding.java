2e9366c3328f1d894578eb8694a37540e52eea61
Lavanya1118 <lavanyak9342@gmail.com>
2025-07-07 22:04:23 +0530

    Create ColourCoding.java

---
+++ ColourCoding.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ColourCoding
+{
+       public static void main(String[] args) {
+	   Scanner s=new Scanner(System.in);
+	   char ch=s.next().charAt(0);
+	   String colour=(ch=='R')?"Red":(ch=='B')?"Blue":(ch=='O')?"Orange":(ch=='Y')?"Yellow":(ch=='W')?"White":"invalid Code";
+	   System.out.println(colour);
+	}
+}
