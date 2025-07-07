c4e2caf21c47aa930b70ad7065a4d832f3b83a7e
Lavanya1118 <lavanyak9342@gmail.com>
2025-07-07 22:40:35 +0530

    Create  SeparateRow.java

---
+++ SeparateRow.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SeparateRow
+{
+   public static void main (String[] args) {
+       for(int i=24;i<=56;i++)
+       {
+           System.out.println(i);
+       }
+    
+}
+}
